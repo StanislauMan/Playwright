ab1c4b3a4dd73486ef3fd6d610e2cff0ba116076
StanislauMan <training.acc44@gmail.com>
2024-02-08 15:47:26 +0300

    updated assertions.spec.js

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('@playwright/test')
-const exp = require('constants')
 
 test ('Assertions', async ({ page }) => {
 
@@ -60,5 +59,5 @@ test ('Assertions', async ({ page }) => {
     const headMenu = await page.locator('.header-menu ul')
 
     await expect(headMenu).toHaveCount(8)
-      
+
 })
