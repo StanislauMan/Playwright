e72daf30ba14a75a3ee472c3298d50f9115f89a1
StanislauMan <training.acc44@gmail.com>
2024-02-12 13:16:53 +0300

    updated dropdown.spec.js

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -6,14 +6,26 @@ test('Dropdown testing', async({ page }) => {
 
     await page.locator('#siblings').selectOption('No') 
 
+    // const options = await page.locator('#siblings option')
+    // await expect(options).toHaveCount(4)
+    
+    // OR 
+
+    // const options = await page.$$('#siblings option')
+    // await expect(options.length).toBe(4)
+
+
+    const content = await page.locator('#siblings').textContent()
+    await expect(content.includes('Yes')).toBeTruthy()
+
     await page.pause()
 })
 
-test.only('Another fropdown', async({ page }) => {
+// test('Another fropdown', async({ page }) => {
 
-    await page.goto('https://testautomationpractice.blogspot.com/')
+//     await page.goto('https://testautomationpractice.blogspot.com/')
 
-    await page.locator('#country').selectOption({label: 'Canada'})
+//     await page.locator('#country').selectOption({label: 'Canada'})
 
-    await page.pause()
-})
+//     await page.pause()
+// })
