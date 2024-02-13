fc1afef7d542ef11fa7b137852297f7dc493cbb3
StanislauMan <training.acc44@gmail.com>
2024-02-13 20:03:07 +0300

    updated alertsWin.spec.js

--- tests/alertsWin.spec.js
+++ tests/alertsWin.spec.js
@@ -1,11 +1,10 @@
 const { test, expect } = require('@playwright/test')
 
-test.skip('Alerts window with OK button', async ({ page }) => {
+test.skip('Alert window with OK button', async ({ page }) => {
 
     await page.goto('https://practice-automation.com/popups/')
 
     const alertBtn = await page.locator('#alert')
-    const promptBtn = await page.locator('#prompt')
 
     //enabling a dialog window handler
     page.on('dialog', async dialog => {
@@ -17,14 +16,13 @@ test.skip('Alerts window with OK button', async ({ page }) => {
     await alertBtn.click()
 })
 
-test('Consirmation window with Cancel and OK buttons', async ({ page }) => {
+test.skip('Consirmation window with Cancel and OK buttons', async ({ page }) => {
 
     await page.goto('https://practice-automation.com/popups/')
 
     const confirmBtn = await page.locator('#confirm')
     
     page.on('dialog', async dialog => {
-
         expect(dialog.type()).toContain('confirm')
         expect(dialog.message()).toEqual('OK or Cancel, which will it be?')
         //await dialog.accept() // close with OK button
@@ -34,4 +32,22 @@ test('Consirmation window with Cancel and OK buttons', async ({ page }) => {
     await confirmBtn.click()
 
     await expect(await page.locator('#confirmResult')).toHaveText('Cancel it is!')
+})
+
+test('Prompt window with input field', async ({ page }) => {
+
+    await page.goto('https://practice-automation.com/popups/')
+
+    const promptBtn = await page.locator('#prompt')
+    let testName = 'userName-01'
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('prompt')
+        expect(dialog.message()).toEqual('Hi there, what\'s your name?')
+        await dialog.accept(testName)
+    })
+
+    await promptBtn.click()
+
+    await expect(await page.locator('#promptResult')).toHaveText(`Nice to meet you, ${testName}!`)
 })
