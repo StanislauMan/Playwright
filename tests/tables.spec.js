01fe6599b180751ecea1da1d6a7d152d6b19355c
StanislauMan <training.acc44@gmail.com>
2024-02-15 14:36:06 +0300

    updated tables.spec.js

--- tests/tables.spec.js
+++ tests/tables.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test')
+const { connect } = require('http2')
 
 test('Tables testing', async ({ page }) => {
 
@@ -12,6 +13,7 @@ test('Tables testing', async ({ page }) => {
     await expect(await columns.count()).toBe(4)
     await expect(await rows.count()).toBe(5)
 
+    // //select an unique product
     // const uniqueRow = rows.filter({
     //     has: page.locator('td'),
     //     hasText: 'Product 4'
@@ -19,11 +21,42 @@ test('Tables testing', async ({ page }) => {
 
     // await matchedRow.locator('input').check()
 
-    await selectProduct(page, rows, 'Product 1')
-    await selectProduct(page, rows, 'Product 2')
-    await selectProduct(page, rows, 'Product 4')
-    await selectProduct(page, rows, 'Product 5')
+    // //select multiple products by re-usable function
+    // await selectProduct(page, rows, 'Product 1')
+    // await selectProduct(page, rows, 'Product 2')
+    // await selectProduct(page, rows, 'Product 4')
+    // await selectProduct(page, rows, 'Product 5')
 
+    //print all product details using for loop
+    // for (let i = 0; i < await rows.count(); i++){
+    //     const row = rows.nth(i)
+    //     const tableData = row.locator('td')
+
+    //     for(let j = 0; j < await tableData.count() - 1; j++){
+
+    //         console.log(await tableData.nth(j).textContent())
+    //     }
+    // }
+
+
+    const pages = await page.locator('#pagination li a')
+
+    for (let k = 0; k < await pages.count(); k++){
+
+        if(k > 0){
+            
+            await pages.nth(k).click()
+        }
+        for (let i = 0; i < await rows.count(); i++) {
+            const row = rows.nth(i)
+            const tableData = row.locator('td')
+
+            for (let j = 0; j < await tableData.count() - 1; j++) {
+
+                console.log(await tableData.nth(j).textContent())
+            }
+        }
+    }
 
 })
 
