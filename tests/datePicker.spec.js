1fb0db9c34165dbddd4597585530b8595e1eff4e
StanislauMan <training.acc44@gmail.com>
2024-02-15 18:48:05 +0300

    updated datePicker.spec.js

--- tests/datePicker.spec.js
+++ tests/datePicker.spec.js
@@ -8,12 +8,44 @@ test ('Data picker testing', async ({ page }) => {
     const dateResult = await page.locator('.field-value')
     const submitBtn = await page.getByRole('button', { name: 'Submit' })
 
-    await dateField.fill('2024-03-10')
+    let year = '2024'
+    let month = 'September'
+    let date = '25'
 
-    await submitBtn.press('Enter')
+    await dateField.click() 
 
-    await page.waitForSelector('.field-value')
+    // await dateField.fill('2024-03-10')
 
-    await expect(dateResult).toHaveText('2024-03-10')
+    // await submitBtn.press('Enter')
+
+    // await page.waitForSelector('.field-value')
+
+    // await expect(dateResult).toHaveText('2024-03-10')
+
+    while(true) {
+
+        const currentMonth = await page.locator('.ui-datepicker-month').textContent()
+        const currentYear = await page.locator('.ui-datepicker-year').textContent()
+
+        if(currentYear == year && currentMonth == month){
+            break
+        }
+
+        await page.locator('a[title="Next"]').click()
+    }
+
+    const dates = await page.$$('a[class="ui-state-default"]')
+
+    for(const day of dates){
+
+        if(await day.textContent() == date){
+            await day.click()
+            break
+        }
+    }
+
+    await submitBtn.click()
+
+    await expect(dateResult).toHaveText('2024-09-25')
 
 })
